e5abba5b1657ffdf43ab805059ff999763a123e3
Prajwal Bawanthare <150644082+prajwal2004-07@users.noreply.github.com>
2024-09-26 20:48:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 console.log("Script loaded");
 const accessKey = "0D7WZR9J2SR18YLVlsUEJZUgR6u-DH-ZN_OjuD27KLI";
 
@@ -69,50 +69,3 @@ searchForm.addEventListener("submit", (e) => {
     page = 1;
     searchImages();
 });
-=======
-const accessKey = "0D7WZR9J2SR18YLVlsUEJZUgR6u-DH-ZN_OjuD27KLI";
-
-const searchForm = document.getElementById("search-form");
-const searchBox = document.getElementById("search-input");
-const searchResult = document.getElementById("search-result");
-const showmoreButton = document.getElementById("show-more-button");
-
-let searchKeyword = "";
-let page = 1;
-
-
-async function searchImages () {
-    searchKeyword = searchBox.value;
-    const url = `https://api.unsplash.com/search/photos?${page}=1&query=${searchKeyword}&client_id=${accessKey}&per_page=12`;
-
-    const response = await fetch(url);
-    const data = await response.json();
-
-    if (page === 1){
-        searchResult.innerHTML = "";
-        showmoreButton.style.display = "none";
-    }
-    const results = data.results;
-
-    results.map(result => {
-        const image = document.createElement("img");
-        image.src = result.urls.small;
-        const imageLink = document.createElement("a");
-        imageLink.href = result.links.html;
-        imageLink.target = "_blank";
-        imageLink.appendChild(image);
-        searchResult.appendChild(imageLink);
-    })
-    showmoreButton.style.display = "block";
-    showmoreButton.addEventListener("click", () => {
-        page++;
-        searchImages();
-    })
-}
-
-searchForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    page = 1;
-    searchImages();
-})
->>>>>>> bba50351c4c4e698eaf1fde13a7ab735ddb032aa
